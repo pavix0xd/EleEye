99463bb685a66d0dcabccdb95a515a16c35b0deb
pavix0xd <minindupavith20@gmail.com>
2025-02-23 09:43:31 +0530

    android ahhhhhhhhhhhhhh

--- mobile-frontend/.dart_tool/dartpad/web_plugin_registrant.dart
+++ mobile-frontend/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: type=lint
 
 import 'package:app_links_web/app_links_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -17,6 +18,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   AppLinksPluginWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   GoogleMapsPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
