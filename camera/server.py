2dd6ed8f85f6ada086458d5b5987797deabf0ab4
Jose <josem@LAPTOP-S9LD4KFB.>
2025-03-09 14:27:16 +0530

    Fixed some typos in the server script and added more comprehensive responses

--- camera/server.py
+++ camera/server.py
@@ -15,17 +15,6 @@ def route(method, path):
     return decorator
 
 def ServerHandler(BaseHTTPRequestHandler):
-
-    def handle_request(self,method):
-        parsed_url = urlparse(self.path)
-        path = parsed_url.path
-
-        if (method in routes) and (path in routes[method]):
-            handler = routes[method][path]
-            handler(self)
-
-        else:
-            self.send_error(404)
     
     def do_GET(self):
         self.handle_request("GET")
@@ -40,28 +29,37 @@ def ServerHandler(BaseHTTPRequestHandler):
         self.handle_request("PUT")
     
     def do_DELETE(self):
-        self.handle("DELETE")
+        self.handle_request("DELETE")
     
     def do_OPTIONS(self):
-        self.handle("OPTIONS")
+        self.handle_request("OPTIONS")
     
     def do_CONNECT(self):
-        self.handle("CONNECT")
+        self.handle_request("CONNECT")
     
     def do_HEAD(self):
-        self.handle("HEAD")
+        self.handle_request("HEAD")
     
     def do_TRACE(self):
-        self.handle("TRACE")
-
+        self.handle_request("TRACE")
     
+
+    def handle_request(self,method):
+        parsed_url = urlparse(self.path)
+        path = parsed_url.path
+
+        if (method in routes) and (path in routes[method]):
+            handler = routes[method][path]
+            handler(self)
+
+        else:
+            self.send_error(404)
+
     def get_payload(self):
-        content_lenght = int(self.headers['Content-lenght'])
-        post_data = self.rfile.read(content_lenght)
+        content_length = int(self.headers['Content-lenght'])
+        post_data = self.rfile.read(content_length)
         return json.loads(post_data.decode('utf-8'))
     
-
-
 def run(server_class=HTTPServer, handler_class=ServerHandler, port=8000):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
@@ -70,7 +68,6 @@ def run(server_class=HTTPServer, handler_class=ServerHandler, port=8000):
 
 
 # functions to validate warning light POST json payload
-
 def validate_color(color):
 
     if not isinstance(color,list) or len(color) != 3:
@@ -88,7 +85,7 @@ def validate_duration(duration):
     if not isinstance(duration, int) or duration <= 0:
         return False, "Duration must be a positive integer"
 
-    elif duration < 20:
+    elif duration > 20:
         return False, "Duration is too long"
 
     return True, None
@@ -99,8 +96,7 @@ def validate_rate(rate):
         return False, "Rate must be a positive integer/float"
 
     return True, None
-
-               
+              
 # routes and methods defined here
 @route('POST', '/warning_light')
 def warning_light(self):
@@ -122,27 +118,52 @@ def warning_light(self):
         if not valid_color or not valid_duration or not valid_rate:
 
             errors = []
+            response = {}
             if color_error: errors.append(color_error)
             if duration_error: errors.append(duration_error)
             if rate_error: errors.append(rate_error)
+
+            response = {
+                "status" : "error",
+                "error" : "Bad Request",
+                "message" : errors
+            }
 
             # Send response with errors
             self.send_response(400) # Bad request
             self.send_header('Content-type','application/json')
             self.end_headers()
-            self.wfile.write(json.dump({'errors' : errors}).encode())
+            self.wfile.write(json.dump(response).encode('utf-8'))
             return
         
         # if Valid data was recieved, the warning light function which interacts
         # with the Raspberry Pi Board will be called, with the passed in parameters
         warning_lights.control_warning_lights(color, duration, rate)
+
+        response = {
+            "status" : "success",
+            "message" : "Warning light turned on",
+            "received_data" : {
+                "color" : color,
+                "duration" : duration,
+                "rate" : rate
+            }
+        }
+
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(b'Warning light turned on')
+        self.wfile.write(json.dumps(response).encode('utf-8'))
     
     except Exception as e:
+
+        response = {
+            "status" : "error",
+            "error" : "Internal Server Error",
+            "message" : "An unexpected error occured on the server.",
+        }
+
         self.send_response(500)
-        self.send_header('Content-type', 'text-plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(str(e).encode())
+        self.wfile.write(json.dump(response).encode('utf-8'))
