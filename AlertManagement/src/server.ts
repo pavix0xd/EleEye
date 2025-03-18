b454ddd2888d50a8337083e9fa31a300822607e0
RanindiFernando <ranindifernando@gmail.com>
2025-03-18 16:35:29 +0530

    Updated server.ts

--- AlertManagement/src/server.ts
+++ AlertManagement/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import * as http from 'http';
 import { Server } from 'socket.io';
-import { notificationRoutes } from './routes/notificationRoutes'; // Import the notification routes
+import { notificationRoutes} from './routes/notificationRoutes'; // Import the notification routes
+import { checkFcmRoutes } from './routes/checkFcmRoutes'; // Import the checkFcm routes
 import * as dotenv from 'dotenv';
 
 dotenv.config();  // Load environment variables
@@ -14,6 +15,7 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 // Set up the route to handle POST /send-notification
 app.use('/api/notifications', notificationRoutes);  // Use the notification routes under '/api/notifications'
+app.use('/api/checkFcm', checkFcmRoutes);  // Use the checkFcm routes under '/api/check-fcm'
 
 // Start the server
 const PORT = process.env.PORT || 5000;
