a1fb23ae663816ffded038d02a2d98b8d1aeda11
Chaniya-2003 <ayukiedahanayake2003@gmail.com>
2025-03-19 20:22:25 +0530

    flutter plugins

--- mobile-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,19 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import firebase_auth
 import firebase_core
+import geolocator_apple
 import google_sign_in_ios
 import location
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
