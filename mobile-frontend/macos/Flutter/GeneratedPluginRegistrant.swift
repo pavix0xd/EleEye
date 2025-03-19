1845605c61f8b55317df88823aeaa149d1ebbc03
Chaniya-2003 <ayukiedahanayake2003@gmail.com>
2025-03-19 21:24:14 +0530

    fix errors

--- mobile-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import geolocator_apple
 import google_sign_in_ios
 import location
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
@@ -21,4 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
