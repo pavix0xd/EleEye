b3b8f6a025a4020ab9cc14733cc1eceaee888cbf
Chaniya-2003 <ayukiedahanayake2003@gmail.com>
2025-03-19 20:25:04 +0530

    fix errors in map dart

--- mobile-frontend/.dart_tool/dartpad/web_plugin_registrant.dart
+++ mobile-frontend/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,8 +6,10 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:device_info_plus/src/device_info_plus_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:location_web/location_web.dart';
@@ -15,8 +17,10 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
   GoogleMapsPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
